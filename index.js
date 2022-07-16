0d650cba7c618bf32a45b74cab079d32f5010783
Parker McKillop <mckilpar000@hotmail.com>
2022-07-15 20:17:34 -0700

    base for generated .md file create, pending work on license badge and license selection

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const inquirer = require("inquirer")
 const fs = require("fs")
 
 // TODO: Create an array of questions for user input
-const questions = ["What is your project title?", 
+const questions = ["What is the title of your project?", 
                    "Briefly describe your project", 
                    "How do you install this project?", 
                    "How do you use this project?", 
                    "Who would you like to include in the credits?", 
                    "What license would you like to use?"];
+
+const licenses = [""]
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -30,29 +32,44 @@ function init() {
             {
                 type: 'input',
                 message: questions[1],
-                name: 'installation',
+                name: 'description',
             },
             {
                 type: 'input',
                 message: questions[2],
-                name: 'usage',
+                name: 'installation',
             },
             {
                 type: 'input',
                 message: questions[3],
-                name: 'credits',
+                name: 'usage',
             },
             {
                 type: 'input',
                 message: questions[4],
+                name: 'credits',
+            },
+            {
+                type: 'list',
+                message: questions[5],
+                choices: ["MIT", "Apache", "GPLV3"],
                 name: 'license',
             }
+           
         ]).then(function(response){
-            fs.writeFile(`${response.filename}.md`, `# ${response.title}\n`, err => err ? console.error(err):console.log("written to file") )
-            fs.appendFile(`${response.filename}.md`, `## Installation\n ${response.installation}\n`, err => err ? console.error(err):console.log("written to file") )
-            fs.appendFile(`${response.filename}.md`, `## Usage\n ${response.usage}\n`, err => err ? console.error(err):console.log("written to file") )
-            fs.appendFile(`${response.filename}.md`, `## Credits\n ${response.credits}\n`, err => err ? console.error(err):console.log("written to file") )
-            fs.appendFile(`${response.filename}.md`, `## license\n ${response.license}\n`, err => err ? console.error(err):console.log("written to file") )
+            fs.writeFile(`${response.filename}.md`, `# ${response.title}\n\n`, err => err ? console.error(err):console.log("File created and Title written to file") )
+
+            fs.appendFile(`${response.filename}.md`, `## Description\n${response.description}\n\n`, err => err ? console.error(err):console.log("Table of contents written to file") )
+
+            fs.appendFile(`${response.filename}.md`, `## Table of Contents\n - [Installation](#installation)\n - [Usage](#usage)\n - [Credits](#credits)\n - [License](#license)\n\n`, err => err ? console.error(err):console.log("Table of contents written to file") )
+
+            fs.appendFile(`${response.filename}.md`, `## Installation\n ${response.installation}\n`, err => err ? console.error(err):console.log("Installation written to file") )
+
+            fs.appendFile(`${response.filename}.md`, `## Usage\n ${response.usage}\n\n`, err => err ? console.error(err):console.log("Usage written to file") )
+
+            fs.appendFile(`${response.filename}.md`, `## Credits\n ${response.credits}\n\n`, err => err ? console.error(err):console.log("Credits written to file") )
+
+            fs.appendFile(`${response.filename}.md`, `## license\n ${response.license}\n\n`, err => err ? console.error(err):console.log("License written to file") )
         })
         
 }
--- readme2.md
+++
@@ -1,9 +0,0 @@
-# Test
-## Usage
- with a little finesse
-## Installation
- Nice little test guy
-## license
- me, myself, and I
-## Credits
- easily
