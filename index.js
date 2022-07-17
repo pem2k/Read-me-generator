7e8ca7aa2cfd52749d04995370514edd78bf713e
Parker McKillop <mckilpar000@hotmail.com>
2022-07-16 19:09:43 -0700

    setup api call for license text, included basic license badge functionality. Pending bugfixes for badge appendation and updated questions

--- index.js
+++ index.js
@@ -1,29 +1,53 @@
-// TODO: Include packages needed for this application
+// required packages
 const inquirer = require("inquirer")
 const fs = require("fs")
+const fetch = require("node-fetch")
 
-// TODO: Create an array of questions for user input
-const questions = ["What is the title of your project?", 
-                   "Briefly describe your project", 
-                   "How do you install this project?", 
-                   "How do you use this project?", 
-                   "Who would you like to include in the credits?", 
-                   "What license would you like to use?"];
+const questions = [
+    "What is the title of your project?",
+    "Briefly describe your project",
+    "How do you install this project?",
+    "How do you use this project?",
+    "Who would you like to include in the credits?",
+    "What license would you like to use?"];
 
-const licenses = [""]
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+//These functions retrieve license text and append them based on user choice
+const mitL = function () {
+    fetch('https://api.github.com/licenses/mit')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data.body)
+            fs.appendFile(`README.md`, `${data.body}`, err => err ? console.error(err) : console.log("MIT License written to file"))
+        })
 
-// TODO: Create a function to initialize app
+}
+
+const apacheL = function () {
+    fetch('https://api.github.com/licenses/apache-2.0')
+        .then(response => response.json())
+        .then(data => {
+            fs.appendFile(`README.md`, `${data.body}`, err => err ? console.error(err) : console.log("Apache License written to file"))
+        })
+
+}
+
+const gplL = function () {
+    fetch('https://api.github.com/licenses/GPL-3.0')
+        .then(response => response.json())
+        .then(data => {
+            fs.appendFile(`README.md`, `${data.body}`, err => err ? console.error(err) : console.log("GPL License written to file"))
+        })
+
+}
+
+//array of licenses
+const licenses = ["MIT", "Apache", "GPLV3"]
+
+
 function init() {
     inquirer
         .prompt([
-            {
-                type: 'input',
-                message: "What would you like to name your file?",
-                name: 'filename',
-            },
             {
                 type: 'input',
                 message: questions[0],
@@ -52,26 +76,50 @@ function init() {
             {
                 type: 'list',
                 message: questions[5],
-                choices: ["MIT", "Apache", "GPLV3"],
+                choices: licenses,
                 name: 'license',
             }
-           
-        ]).then(function(response){
-            fs.writeFile(`${response.filename}.md`, `# ${response.title}\n\n`, err => err ? console.error(err):console.log("File created and Title written to file") )
 
-            fs.appendFile(`${response.filename}.md`, `## Description\n${response.description}\n\n`, err => err ? console.error(err):console.log("Table of contents written to file") )
+        ]).then(function (response) {
+            fs.writeFile(`README.md`, `# ${response.title}\n\n`, err => err ? console.error(err) : console.log("File created and Title written to file"))
 
-            fs.appendFile(`${response.filename}.md`, `## Table of Contents\n - [Installation](#installation)\n - [Usage](#usage)\n - [Credits](#credits)\n - [License](#license)\n\n`, err => err ? console.error(err):console.log("Table of contents written to file") )
+            if (response.license == "MIT") {
+                fs.appendFile(`README.md`, `![License Badge](https://img.shields.io/badge/license-MIT-green)\n\n`, err => err ? console.error(err) : console.log("File created and Title written to file"))
+            }
+            else if (response.license == "Apache") {
+                fs.appendFile(`README.md`, `![License Badge](https://img.shields.io/badge/License-Apache%202.0-green)\n\n`, err => err ? console.error(err) : console.log("File created and Title written to file"))
+            }
+            else {
+                fs.appendFile(`README.md`, `![License Badge](https://img.shields.io/badge/License-GPL%203-green)\n\n`, err => err ? console.error(err) : console.log("File created and Title written to file"))
+            }
 
-            fs.appendFile(`${response.filename}.md`, `## Installation\n ${response.installation}\n`, err => err ? console.error(err):console.log("Installation written to file") )
+            fs.appendFile(`README.md`, `## Description\n${response.description}\n\n`, err => err ? console.error(err) : console.log("Table of contents written to file"))
 
-            fs.appendFile(`${response.filename}.md`, `## Usage\n ${response.usage}\n\n`, err => err ? console.error(err):console.log("Usage written to file") )
+            fs.appendFile(`README.md`, `## Table of Contents\n - [Installation](#installation)\n - [Usage](#usage)\n - [Credits](#credits)\n - [License](#license)\n\n`, err => err ? console.error(err) : console.log("Table of contents written to file"))
 
-            fs.appendFile(`${response.filename}.md`, `## Credits\n ${response.credits}\n\n`, err => err ? console.error(err):console.log("Credits written to file") )
+            fs.appendFile(`README.md`, `## Installation\n ${response.installation}\n`, err => err ? console.error(err) : console.log("Installation written to file"))
 
-            fs.appendFile(`${response.filename}.md`, `## license\n ${response.license}\n\n`, err => err ? console.error(err):console.log("License written to file") )
+            fs.appendFile(`README.md`, `## Usage\n ${response.usage}\n\n`, err => err ? console.error(err) : console.log("Usage written to file"))
+
+            fs.appendFile(`README.md`, `## Credits\n ${response.credits}\n\n`, err => err ? console.error(err) : console.log("Credits written to file"))
+
+            fs.appendFile(`README.md`, `## license\n ${response.license}\n\n`, err => err ? console.error(err) : console.log("License written to file"))
+
+            if (response.license == "MIT") {
+
+                mitL()
+
+            } else if (response.license == "Apache") {
+
+                apacheL()
+
+            } else {
+
+                gplL()
+
+            }
         })
-        
+
 }
 
 // Function call to initialize app
